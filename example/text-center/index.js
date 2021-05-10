17c8e966e923819ae70cb9f480cdea45dd6a8fb4
dezhizhang <1541609448@qq.com>
2021-05-10 22:56:05 +0800

    update

---
+++ example/text-center/index.js
@@ -0,0 +1,9 @@
+import './index.less';
+
+const btn = document.getElementById('btnMore');
+const content = document.getElementById('conMore');
+if(btn && conMore) {
+    btn.onclick = function() {
+        content.innerHTML += '新增文字'
+    }
+}
