c73018c977af1e4903942cdc4315dea384e0d26e
dezhizhang <1541609448@qq.com>
2021-05-17 08:02:56 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2021-05-09 23:02:51
- * @LastEditTime: 2021-05-12 21:12:28
+ * @LastEditTime: 2021-05-17 08:00:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /css/src/index.js
  */
 import './index.less';
+
+
+var eleLabel = document.querySelector('label[for="search"]'),
+eleSearch = document.getElementById('search');
+
+if (eleLabel && eleSearch) {
+    eleLabel.onclick = function() {
+        eleSearch.value = '';
+    };
+}
