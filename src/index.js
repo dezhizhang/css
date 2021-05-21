a7d6b8d84f596e9f21f6f5d59ac9e06cb1fad7e0
dezhizhang <1541609448@qq.com>
2021-05-21 20:50:53 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-05-19 22:04:05
  * :last editor: 张德志
- * :date last edited: 2021-05-19 22:04:05
+ * :date last edited: 2021-05-21 20:12:19
  */
 import './index.less'
